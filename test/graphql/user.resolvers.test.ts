43c53839c25558562c6574531e9ce007d1f79e3a
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-08-09 18:12:58 -0300

    Testes unitários usando Jest

--- test/graphql/user.resolvers.test.ts
+++ test/graphql/user.resolvers.test.ts
@@ -1,226 +1,180 @@
-import {
-  app, chai, expect, handlerGQLError
-} from '../test-utils'
-import {
-  dateOnly
-} from '../../src/utils/index'
-describe('GraphQL', () => {
-  let userId = null
-  describe('Mutation', () => {
-    describe('saveUser', () => {
-      it('Should create a user', async () => {
-        let body = {
-          query: `
-            mutation {
-              saveUser(input: {
-                name: "Star Wars: The New Hope",
-                birthday: "${dateOnly(new Date('1977-11-18'))}"
-              }) {
-                id
-                name
-                birthday
-                createAt
-                updateAt
-              }
-            }
-          `
+import { app, request, handlerGQLError } from '../config'
+import { dateOnly } from '../../src/utils/index'
+
+let user = null
+
+describe('User', () => {
+  test('Should return a user list', async () => {
+    let body = {
+      query: `
+        query List {
+          users { id name}
         }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { saveUser } = res.body.data
-        userId = saveUser.id
-        expect(saveUser).to.be.an('object')
-        expect(saveUser.createAt).to.be.an('string')
-        expect(saveUser).to.deep.equal({
-          id: userId,
-          name: 'Star Wars: The New Hope',
-          birthday: new Date('1977-11-18').toISOString(),
-          createAt: saveUser.createAt,
-          updateAt: null
-        })
-      })
-      it('Should update user', async () => {
-        let body = {
-          query: `
-            mutation {
-              saveUser(id: "${userId}", input: {
-                name: "Star Wars: The Empire Strikes Back",
-                birthday: "${dateOnly(new Date('1980-07-21'))}"
-              }) {
-                id
-                name
-                birthday
-                updateAt
-              }
-            }
-          `
+      `
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { users } = res.body.data
+    expect(Array.isArray(users)).toEqual(true)
+  })
+  test('Should create a user', async () => {
+    let body = {
+      query: `
+        mutation Create($input: UserInput!){
+          saveUser(input: $input) {
+            id name birthday createAt updateAt
+          }
         }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { saveUser } = res.body.data
-        expect(saveUser).to.be.an('object')
-        expect(saveUser.updateAt).to.be.an('string')
-        expect(saveUser).to.deep.equal({
-          id: userId,
-          name: 'Star Wars: The Empire Strikes Back',
-          birthday: new Date('1980-07-21').toISOString(),
-          updateAt: saveUser.updateAt
-        })
-      })
+      `,
+      variables: {
+        input: {
+          name: 'Star Wars: A new hope',
+          birthday: '1997-11-18'
+        }
+      }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { saveUser } = res.body.data
+    expect(saveUser).not.toBeNull()
+    expect(typeof saveUser).toEqual('object')
+    expect(saveUser).toMatchObject({
+      ...body.variables.input
+    })
+    user = { ...saveUser }
+  })
+  test('Should return a user list filtered by name', async () => {
+    let body = {
+      query: `
+        query List($name: String) {
+          users(name: $name) {
+            id
+            name
+            birthday
+            createAt
+            updateAt
+          }
+        }
+      `,
+      variables: { name: user.name }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { users } = res.body.data
+    expect(Array.isArray(users)).toEqual(true)
+    expect(users[0]).toBeTruthy()
+  })
+  test('Shouldn\'t find any user', async () => {
+    let body = {
+      query: `
+        query Read($id: ID!) {
+          find: user(id: $id) { id }
+        }
+      `,
+      variables: { id: "0" }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { find } = res.body.data
+    expect(find).toBeNull()
+  })
+  test('Should find an user', async () => {
+    let body = {
+      query: `
+        query Read($id: ID!) {
+          find: user(id: $id) {
+            id name birthday createAt updateAt
+          }
+        }
+      `,
+      variables: { id: user.id }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { find } = res.body.data
+    expect(find).not.toBeNull()
+    expect(typeof find).toEqual('object')
+    expect(find).toMatchObject(user)
+  })
+  test('Should update an user', async () => {
+    let body = {
+      query: `
+        mutation Update($id: ID, $input: UserInput!) {
+          saveUser(id: $id, input: $input) {
+            id name birthday createAt updateAt
+          }
+        }
+      `,
+      variables: {
+        id: user.id,
+        input: {
+          name: 'Star Wars: The empire strikes back',
+          birthday: '1980-06-21'
+        }
+      }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { saveUser } = res.body.data
+    expect(saveUser).not.toBeNull()
+    expect(saveUser.updateAt).not.toBeNull()
+    expect(saveUser).toMatchObject({
+      id: user.id,
+      ...body.variables.input
     })
   })
-  describe('Query', () => {
-    describe('users', () => {
-      it('Should query users', async () => {
-        let body = {
-          query: `
-            query {
-              users {
-                id
-                name
-              }
-            }
-          `
+  test('Should fail trying to delete an user', async () => {
+    let body = {
+      query: `
+        mutation Delete($id: ID!) {
+          deleteUser(id: $id)
         }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { users } = res.body.data
-        expect(users).to.be.an('array')
-        expect(users.length).to.greaterThan(0)
-        expect(users[0]).to.be.an('object')
-      })
-      it('Should found users by name', async () => {
-        let body = {
-          query: `
-            query {
-              users(name: "a") {
-                id
-                name
-              }
-            }
-          `
-        }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { users } = res.body.data
-        expect(users).to.be.an('array')
-        expect(users.length).to.greaterThan(0)
-      })
-      it('Should\'t found users by name', async () => {
-        let body = {
-          query: `
-            query {
-              users(name: "wxyz87") {
-                id
-                name
-              }
-            }
-          `
-        }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { users } = res.body.data
-        expect(users).to.be.an('array')
-        expect(users.length).to.equal(0)
-      })
-    })
-    describe('user', () => {
-      it('Should\'t find user by ID '+
-        '(return \'User not found\')', async () => {
-        let body = {
-          query: `
-            query {
-              user(id: "-1") {
-                id
-                name
-              }
-            }
-          `
-        }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-        const { errors } = res.body
-        expect(errors).to.be.an('array')
-        expect(errors[0].message).to.equal('User not found')
-      })
-      it('Should find user by ID', async () => {
-        let body = {
-          query: `
-            query {
-              user(id: "${userId}") {
-                id
-                name
-                birthday
-              }
-            }
-          `
-        }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { user } = res.body.data
-        expect(user).to.be.an('object')
-        expect(user).to.deep.equal({
-          id: userId,
-          name: 'Star Wars: The Empire Strikes Back',
-          birthday: new Date('1980-07-21').toISOString()
-        })
-      })
-    })
+      `,
+      variables: { id: "0" }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { deleteUser } = res.body.data
+    expect(typeof deleteUser).toEqual('boolean')
+    expect(deleteUser).toEqual(false)
   })
-  describe('Mutation', () => {
-    describe('deleteUser', () => {
-      it('Should delete user with false', async () => {
-        let body = {
-          query: `
-            mutation {
-              deleteUser(id: "-1")
-            }
-          `
+  test('Should delete an user', async () => {
+    let body = {
+      query: `
+        mutation Delete($id: ID!) {
+          deleteUser(id: $id)
         }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { deleteUser } = res.body.data
-        expect(deleteUser).to.equal(false)
-      })
-      it('Should delete user with true', async () => {
-        let body = {
-          query: `
-            mutation {
-              deleteUser(id: ${userId})
-            }
-          `
-        }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { deleteUser } = res.body.data
-        expect(deleteUser).to.equal(true)
-      })
-    })
+      `,
+      variables: { id: user.id }
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { deleteUser } = res.body.data
+    expect(typeof deleteUser).toEqual('boolean')
+    expect(deleteUser).toEqual(true)
   })
 })
--- test/graphql/version.resolvers.test.ts
+++ test/graphql/version.resolvers.test.ts
@@ -1,29 +1,32 @@
-import {
-  app,
-  chai,
-  expect,
-  handlerGQLError
-} from '../test-utils'
+import { app, request, handlerGQLError } from '../config'
 
-describe('GraphQL', () => {
-  describe('Queries', () => {
-    describe('version', () => {
-      it('Should return the API version', async () => {
-        let body = {
-          query: `
-          query {
-            version
-          }`
-        }
-        const res = await chai.request(app)
-          .post('/graphql')
-          .set('content-type', 'application/json')
-          .send(JSON.stringify(body))
-          .then(handlerGQLError)
-        const { version } = res.body.data
-        expect(version).to.be.an('string')
-        expect(version).to.match(/^\d+\.\d+\.\d+/)
-      })
-    })
+describe('Version', () => {
+  test('Should return a GraphQL error', async () => {
+    let body = {
+      query: `
+        mutation { version }
+      `
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+    const { errors } = res.body
+    expect(Array.isArray(errors)).toEqual(true)
+    expect(errors[0].message).toEqual('Cannot query field \"version\" on ' +
+      'type \"Mutation\".')
+  })
+  test('Should return the API version', async () => {
+    let body = {
+      query: `query { version }`
+    }
+    const res = await request(app)
+      .post('/graphql')
+      .set('content-type', 'application/json')
+      .send(JSON.stringify(body))
+      .then(handlerGQLError)
+    const { version } = res.body.data
+    expect(typeof version).toEqual('string')
+    expect(version).toMatch(/^\d+\.\d+\.\d+/)
   })
 })
